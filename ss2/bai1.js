e3413a8c05ec6ccc1690240b682557f238c62c6d
ThanhPhu412 <149989622+ThanhPhu412@users.noreply.github.com>
2024-05-15 00:01:23 +0700

    p

---
+++ ss2/bai1.js
@@ -0,0 +1,10 @@
+let student = [
+    { id: 1, name: "Mun" },
+    { id: 2, name: "Lu" },
+    { id: 3, name: "Van" },
+    { id: 4, name: "Luu" },
+  ];
+  
+  for (let key in student) {
+    console.log(student[key].name);
+  }
---
+++ ss3/bai3.js
@@ -0,0 +1,10 @@
+let student=[
+  {id:1, name:"DDD"},
+  {id:2, name:"aaa"},
+  {id:3, name:"bbbb"},
+  {id:4, name:"kccc"},
+]
+
+student.forEach(stu=>{
+  console.log(`Xin chao !!! ${stu.name}`);
+})
