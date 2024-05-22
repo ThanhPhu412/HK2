401b4212970ae699e18af16153fc85b5355626be
ThanhPhu412 <149989622+ThanhPhu412@users.noreply.github.com>
2024-05-22 23:04:06 +0700

    s

---
+++ ss10/bt3/bai3.js
@@ -0,0 +1,6 @@
+"use strict";
+let studen = {
+    username: "van",
+    age: 20,
+};
+console.log(studen.username + " " + studen.age);
---
+++ ss10/bt5/bai5.js
@@ -0,0 +1,3 @@
+"use strict";
+let person = ["van", 10];
+console.log(person);
